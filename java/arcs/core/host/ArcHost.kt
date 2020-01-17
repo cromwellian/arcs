546710518ccc9c8750c92a8adde76ccae0cfd137
Ray Cromwell <cromwellian@gmail.com>
2020-01-17 14:26:09 -0800

    Initial Allocator implementation

--- java/arcs/core/host/ArcHost.kt
+++ java/arcs/core/host/ArcHost.kt
@@ -24,7 +24,7 @@ interface ArcHost {
     /**
      * Registers a [Particle] class with this host.
      */
-    fun registerParticle(particles: KClass<out Particle>): Unit
+    fun registerParticle(particle: KClass<out Particle>): Unit
 
     /**
      * Unregisters a [Particle] class.
@@ -35,5 +35,18 @@ interface ArcHost {
      * Returns a list of Particles registered to run in this host.
      */
     val registeredParticles: List<KClass<out Particle>>
-    // TODO: Implement startArc/stopArc/handleMessage
+
+    /**
+     * Requests this arc host to start or restart an Arc associated with this [PlanPartition]. This
+     * may include creating handles and storage proxies, registering for updates from the storage
+     * system, instantiating particles, and registering for resurrection.
+     */
+    fun startArc(partition: PlanPartition)
+
+    /**
+     * Shuts down an existing arc. This may include unregistering for updates from storage,
+     * resurrection, and notifying particles they are at the end of their lifecycle.
+     */
+    fun stopArc(partition: PlanPartition)
+    // TODO: HandleMessage
 }
